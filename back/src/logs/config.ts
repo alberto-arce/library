7041a2ae463aed03b619725d320aa5b7c64810ee
Alberto-Arcentales <alberto.a@embluemail.com>
2023-12-05 10:58:41 -0300

    Add log service
    This compromise presents the solution of log service not found
    
    feat: add log service

---
+++ back/src/logs/config.ts
@@ -0,0 +1,14 @@
+import { createLogger, transports, format } from 'winston';
+
+export const logger = createLogger({
+  level: 'info',
+  format: format.combine(
+    format.timestamp(),
+    format.printf(({ level, message, timestamp }) => {
+      return `${timestamp} ${level}: ${message}`;
+    })
+  ),
+  transports: [
+    new transports.File({ filename: `${__dirname}/api.log` }) 
+  ]
+});
